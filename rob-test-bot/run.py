c8a2b36c9e1672cd886bd1756ef1548e7f9cc006
Robert Hausch <rob.hausch@gmail.com>
2018-01-11 20:30:40 -0800

    merge stuff:

--- rob-test-bot/run.py
+++ rob-test-bot/run.py
@@ -26,6 +26,10 @@ gc.queue_research(bc.UnitType.Knight)
 gc.queue_research(bc.UnitType.Knight)
 
 my_team = gc.team()
+if my_team == bc.Team.Red:
+    opponent_team = bc.Team.Blue
+else:
+    opponent_team = bc.Team.Red
 
 while True:
     # We only support Python 3, which means brackets around print()
@@ -38,6 +42,8 @@ while True:
     myRangers = []
     myRockets = []
 
+    someLoc = None
+
     # frequent try/catches are a good idea
     try:
         # walk through our units:
@@ -48,9 +54,12 @@ while True:
                 myWorkers.append(unit)
             elif unit.unit_type == bc.UnitType.Knight:
                 myKnights.append(unit)
+
+            if someLoc is None and unit.location.is_on_map():
+                someLoc = unit.location.map_location()
         print('pyround:', gc.round(),
               ' karbonite:', gc.karbonite(),
-              ' units:', len(myWorkers), ',', len(myFactories), ',', len(myKnights))
+              ' units:', len(myWorkers), ',', len(myFactories), ',', len(myKnights), ' debug:', someLoc)
 
         if len(myWorkers) < 5 and gc.karbonite() > 16:
             print('Not enough workers:', gc.karbonite())
@@ -61,28 +70,32 @@ while True:
                     print('replicated! ', gc.karbonite())
                     break
 
-        if gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and len(myFactories) < 2:
+        if gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and (len(myFactories) < 2 or gc.karbonite() > 300):
             d = random.choice(directions)
             for worker in myWorkers:
                 if gc.can_blueprint(worker.id, bc.UnitType.Factory, d):
                     gc.blueprint(worker.id, bc.UnitType.Factory, d)
                     break
 
+        factoriesToHeal = []
         for factory in myFactories:
+            if factory.health < factory.max_health:
+                factoriesToHeal.append(factory)
             garrison = factory.structure_garrison()
-            print('in factory:', factory.id, ' ', len(garrison), ' ', factory.unit_type, ' ', gc.can_produce_robot(factory.id, bc.UnitType.Knight))
+            # print('in factory:', factory.id, ' ', len(garrison), ' ', factory.unit_type, ' ', gc.can_produce_robot(factory.id, bc.UnitType.Knight))
             if len(garrison) > 0:
                 d = random.choice(directions)
                 if gc.can_unload(factory.id, d):
-                    print('unloaded a knight!')
+                    # print('unloaded a knight!')
                     gc.unload(factory.id, d)
                     continue
             elif gc.can_produce_robot(factory.id, bc.UnitType.Knight):
                 gc.produce_robot(factory.id, bc.UnitType.Knight)
-                print('produced a knight!')
+                # print('produced a knight!')
                 continue
 
         # Have workers move to
+        # TODO: handle being in a garrison
         for worker in myWorkers:
             location = worker.location
             if location.is_on_map():
@@ -90,18 +103,47 @@ while True:
                 for other in nearby:
                     if gc.can_build(worker.id, other.id):
                         gc.build(worker.id, other.id)
-                        print('built a factory!')
+                        # print('built a factory!')
                         # skip moving
                         continue
+                    if gc.can_repair(worker.id, other.id):
+                        gc.repair(worker.id, other.id)
+                        print('repaired a factory!')
+                        continue
+            if len(factoriesToHeal) > 0: #move towards closest factory
+                closestFactory = None
+                distance = 999
+                mapLocation = location.map_location()
+                for factory in factoriesToHeal:
+                    loc = factory.location.map_location()
+                    dist = mapLocation.distance_squared_to(loc)
+                    if dist < distance:
+                        distance = dist
+                        closestFactory = factory
+                if closestFactory is not None:
+                    # print("Moving to closest factory:", dist)
+                    d = mapLocation.direction_to(factory.location.map_location())
+                    if gc.is_move_ready(worker.id) and gc.can_move(worker.id, d):
+                        gc.move_robot(worker.id, d)
+            else: #move randomly
+                d = random.choice(directions)
+                if gc.is_move_ready(worker.id) and gc.can_move(worker.id, d):
+                    gc.move_robot(worker.id, d)
 
-            d = random.choice(directions)
-            if gc.is_move_ready(worker.id) and gc.can_move(worker.id, d):
-                gc.move_robot(worker.id, d)
+        enemy_locations = []
+        #sense enemies
+        if someLoc is not None:
+            units = gc.sense_nearby_units_by_team(someLoc, 5001, opponent_team)
+            print('enemies sensed:', len(units))
+            for unit in units:
+                if unit.location.is_on_map():
+                    enemy_locations.append(unit.location.map_location())
 
         # moves knights around randomly
         for knight in myKnights:
             location = knight.location
             if location.is_on_map():
+                mapLoc = location.map_location()
                 nearby = gc.sense_nearby_units(location.map_location(), 2)
                 for other in nearby:
                     if other.team != my_team and gc.is_attack_ready(knight.id) and gc.can_attack(knight.id, other.id):
@@ -109,9 +151,25 @@ while True:
                         gc.attack(knight.id, other.id)
                         continue
 
-            d = random.choice(directions)
-            if gc.is_move_ready(knight.id) and gc.can_move(knight.id, d):
-                gc.move_robot(knight.id, d)
+                if len(enemy_locations) > 0:
+                    closestLoc = None
+                    distance = 999
+                    mapLocation = location.map_location()
+                    for loc in enemy_locations:
+                        dist = mapLocation.distance_squared_to(loc)
+                        if dist < distance:
+                            distance = dist
+                            closestLoc = loc
+                    if closestLoc is not None:
+                        # print("Moving to closest factory:", dist)
+                        d = mapLocation.direction_to(closestLoc)
+                        if gc.is_move_ready(knight.id) and gc.can_move(knight.id, d):
+                            gc.move_robot(knight.id, d)
+
+                else:
+                    d = random.choice(directions)
+                    if gc.is_move_ready(knight.id) and gc.can_move(knight.id, d):
+                        gc.move_robot(knight.id, d)
 
 
     except Exception as e:
